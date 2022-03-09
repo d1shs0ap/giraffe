a3188f9412bd9150c606b3552a031b1763ad0e24
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2022-03-09 05:22:21 -0500

    basic version of search working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Layout, Input } from 'antd';
+import { useState, useMemo } from 'react';
+import { MapContainer, TileLayer, useMap, Polyline } from 'react-leaflet';
+import { Layout, Input, List } from 'antd';
 import './App.css';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Search } = Input;
 
+function Rivers({results}) {
+  return (
+    <>
+      {results?.map((result, i) => <Polyline positions={result.geometry} />)}
+    </>
+  )
+}
+
+
 function App() {
+  const [results, setResults] = useState();
+  const [loading, setLoading] = useState(false);
+
   async function onSearch(value) {
+    setLoading(true);
+    
     const response = await fetch('http://localhost:5000/', {
       method: 'POST',
       headers: {
@@ -18,21 +33,38 @@ function App() {
       })
     });
     const data = await response.json();
+    
     console.log(data);
+    setResults(data);
+   
+    setLoading(false);
   }
 
   return (
     <div className="App">
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider width={400}>
-          <Search placeholder="input search text" onSearch={onSearch} style={{ width: 400 }} />
+        <Sider width={400} theme='light'>
+          <Search placeholder="input search text" onSearch={onSearch} loading={loading} style={{ width: 380, marginRight: 10, marginLeft: 10, marginTop: 20 }} />
+          <List
+            dataSource={results}
+            style={{ margin: 10 }}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  title={item.contents}
+                  description={item.description}
+                />
+              </List.Item>
+            )}
+          />
         </Sider>
         <Layout className="site-layout">
-          <MapContainer center={[51.505, -0.09]} zoom={13}>
+          <MapContainer center={[43.4643, -80.5204]} zoom={13}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Rivers results={results}/>
           </MapContainer>
         </Layout>
       </Layout>
--- server/app.py
+++ server/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 import sys
 sys.path.insert(1, '../../pyserini')
+sys.path.insert(1, '../../watersheds')
 
 from pyserini.search.lucene import LuceneGeoSearcher
-from pyserini.search.lucene._geo_searcher import JSort, JLatLonDocValuesField, JLatLonShape, JQueryRelation
+from pyserini.search.lucene._geo_searcher import JSort, JLatLonDocValuesField, JLatLonShape, JQueryRelation, JLongPoint
+from pyserini.search.lucene import LuceneSearcher
+
+from shapely.geometry import Point
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import contextily as cx
+from watersheds._base import Basin, River
 
 app = Flask(__name__)
 CORS(app)
@@ -15,35 +24,100 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def rivers_search():
   if request.method == 'POST':
     req = request.json
-    
-    print(req['searchText'])
-
-    # get mouths of river coords from req['searchText']. list of [lat, lon]
-    
-
-    # get river segments from mouths of geo coords
-    searcher = LuceneGeoSearcher('indexes/hydrorivers')
-    query = JLatLonShape.newBoxQuery("geometry", JQueryRelation.INTERSECTS, 43, 44, -78, -77);
-    hits = searcher.search(query)
-    print(hits)
-    # trace upstream to get entire rivers
-    
-
-    # return json containing matching rivers coords and metadata
-    return jsonify({"hi": "bye"})
+    results = getGeometries(req['searchText'])
+    return jsonify(results)
   
   elif request.method == 'GET':
     return "This is the endpoint for geo search."
 
+def getGeometries(text):
+  # get rivers and their mouths from text
+  print("Searching for rivers in wiki...")
+  searcher = LuceneSearcher('indexes/wikidata')
+  hits = searcher.search(text, fields={'contents': 1.0}, k=5)
+  
+  # convert raw string results to json
+  print("Converting string results to json...")
+  results = []
+  for i in range(len(hits)):
+    raw = json.loads(hits[i].raw)
+    results.append(raw)
+  
+  # get geometries of each river
+  print("Getting geometries of rivers...")
+  searcher = LuceneGeoSearcher('indexes/hydrorivers')
+  
+  for result in results:
+    print(f"Getting geometry of {result['contents']}...")
 
-def props(cls):   
-  return [i for i in cls.__dict__.keys() if i[:1] != '_']
+    # query for initial segment
+    query = JLatLonShape.newBoxQuery("geometry", JQueryRelation.INTERSECTS, -90, 90, -180, 180)
+    sort = JSort(JLatLonDocValuesField.newDistanceSort("point", result['details']['coordinate'][0][1], result['details']['coordinate'][0][0]))
+    hits = searcher.search(query, 1, sort)
+
+    # trace segment down
+    wkts = []
+    cur_segment = json.loads(hits[0].raw)
+    
+    while cur_segment['NEXT_DOWN'] != 0:
+      wkts.append(cur_segment['geometry'])
+      
+      nextQuery = JLongPoint.newExactQuery('HYRIV_ID', cur_segment['NEXT_DOWN'])
+      hits = searcher.search(nextQuery, 1)
+      cur_segment = json.loads(hits[0].raw)
+    
+    wkts.append(cur_segment['geometry'])
+
+    # WKT string -> Shapely shape -> coordinates of each segment[]
+    segments = gpd.GeoSeries.from_wkt(wkts)
+    # reverse the coordinates to be [lat, lon]
+    result['geometry'] = [[[p[1], p[0]] for p in list(line.coords)] for line in segments]
+
+    # set zoom bounds, first point bottom left and second point top right
+    result['bounds'] = [ 
+      [min([min(line, key=lambda p: p[0]) for line in result['geometry']], key=lambda p: p[0])[0], min([min(line, key=lambda p: p[1]) for line in result['geometry']], key=lambda p: p[1])[1]],
+      [max([max(line, key=lambda p: p[0]) for line in result['geometry']], key=lambda p: p[0])[0], max([max(line, key=lambda p: p[1]) for line in result['geometry']], key=lambda p: p[1])[1]]
+    ]
+
+  return results
 
 if __name__ == "__main__":
-  searcher = LuceneGeoSearcher('indexes/hydrorivers')
-  print(props(JQueryRelation))
+  getGeometries('Ob')
 
-  query = JLatLonShape.newBoxQuery("geometry", JQueryRelation.INTERSECTS, 43, 44, -78, -77)
-  sort = JSort(JLatLonDocValuesField.newDistanceSort("point", -35, 0))
-  hits = searcher.search(query, 2, sort)
-  print(hits[0].raw)
+  # na_basin = Basin()
+  # na_river = River()
+
+  # res = []
+  
+  # for result in results:
+  #   # process river only if we have both mouth and source
+  #   if len(result['details']['coordinate']) < 2:
+  #     continue
+    
+  #   # put mouth and source into Point
+  #   stl_river_mouth_point = Point(result['details']['coordinate'][0][0], result['details']['coordinate'][0][1])
+  #   stl_river_source_point = Point(result['details']['coordinate'][1][0], result['details']['coordinate'][1][1])
+
+  #   # get basins near mouth and source
+  #   result_mouth = na_basin.find_point_belongs_to(stl_river_mouth_point)
+  #   result_source = na_basin.find_point_belongs_to(stl_river_source_point)
+
+  #   # if they don't exist (probably because not in NA, skip)
+  #   if not result_mouth or not result_source:
+  #     continue
+
+  #   # Get all basins and river IDs in those basins
+  #   all_btw_basins = na_basin.find_basins_btw_source_mouth(result_source[0], result_mouth[0])
+  #   all_btw_rivers = na_river.get_rivers_id_in_basins(all_btw_basins)
+
+  #   # Get all coordinates using river IDs
+  #   geo_stlr_rivers = [gpd.GeoSeries(na_river.get_geo_by_id(item)) for item in all_btw_rivers]
+  #   print(na_river.get_geo_by_id(all_btw_rivers[0]))
+    
+  #   result['geometry'] = geo_stlr_rivers
+  #   res.append(result)
+  
+  # print(res)
+
+
+
